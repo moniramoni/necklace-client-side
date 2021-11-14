6c4e3fa8858e7732beb7fb662438fb81087f9fa9
moniramoni <moniinfo60@gmail.com>
2021-11-14 15:23:03 +0600

    rating

--- src/Pages/Home/ReviewCollections/ReviewCollections.js
+++ src/Pages/Home/ReviewCollections/ReviewCollections.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 // import useAuth from '../../../hooks/useAuth';
+import Rating from 'react-rating'
 
 const ReviewCollections = () => {
     // const {user} = useAuth()
     const [reviewCollections, setReviewCollection] = useState([])
 
     useEffect(() => {
-        fetch("http://localhost:5000/reviews")
+        fetch("https://guarded-shelf-19111.herokuapp.com/reviews")
         .then(res => res.json())
         .then(result => {
             setReviewCollection(result);
@@ -15,17 +16,18 @@ const ReviewCollections = () => {
 
     return (
         <>
-            <h2>Review Collections: {reviewCollections.length}</h2>
+            <h1 className="my-5"> Review Collections</h1>
             <div className="m-auto mb-5">
                 <div className="row container  m-auto">
                     {reviewCollections?.map((reviewCollection) => (
                         <div className="col-md-6 col-lg-4 col-12 text-start" key= {reviewCollection._id}>
                             <div className="mb-4">
                                 <div className="text-center">
-                                    <img className="profile-img border border-2 card-border shadow" src={reviewCollection.featuredImage} alt="" />
+                                    <img className="profile-img border border-2 shadow" src={reviewCollection.featuredImage} alt="" />
                                     <h4 className="pt-2 fs-6 px-3 color-orange">{reviewCollection.name}</h4>
                                     <hr className="w-25 m-3 text-center m-auto" />
-                                    <p className="p-2 px-3">{reviewCollection.comments}</p>
+                                    <p className="pt-2 px-3">{reviewCollection.comments}</p>
+                                    <Rating initialRating={reviewCollection.rating} emptySymbol="far fa-star color-orange" fullSymbol="fas fa-star color-orange" readonly></Rating>
                                     <hr className="w-100 m-3" />
                                 </div>
                             </div>
